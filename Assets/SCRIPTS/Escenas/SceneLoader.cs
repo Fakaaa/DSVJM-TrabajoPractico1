6db078252862e6db7878aa463faa75f4120ccf25
FacundoPonce <mrfacu125@gmail.com>
2021-09-09 23:15:07 -0300

    Created scene loader with singleton pattern.

---
+++ Assets/SCRIPTS/Escenas/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private static SceneLoader instance;
+
+    public static SceneLoader Get()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        Scene auxSene = SceneManager.GetSceneByName(sceneName);
+        if(auxSene != null)
+            SceneManager.LoadScene(sceneName);
+    }
+    public void LoadScene(int indexScene)
+    {
+        Scene auxSene = SceneManager.GetSceneByBuildIndex(indexScene);
+        if(auxSene != null)
+            SceneManager.LoadScene(indexScene);
+    }
+}
